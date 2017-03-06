2c640c8abf39516d835b6eaad8821820d433dc36
Rob Hardt <rhardt@pivotal.io>
2017-03-06 11:39:46 -0800

    setting environment vars based on sidecar's /sidecar endpoint

---
+++ resources/sidecar/export_sidecar_config.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+# Encoding: utf-8
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+$stdout.sync = true
+$stderr.sync = true
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'net/http'
+require 'json'
+
+port = ENV['SIDECAR_PROXY_PORT'] || 8087
+
+retries = 36
+response = nil
+url = "http://127.0.0.1:#{port}/sidecar"
+uri = URI(url)
+
+begin
+  response = Net::HTTP.get(uri)
+rescue Exception
+  sleep 5
+  retry if (retries -= 1) > 0
+end
+
+def envify(s)
+  return s.upcase.gsub(".", "_").gsub("-", "_")
+
+end
+
+if response
+  resp_obj = JSON.parse(response)
+  open('./sidecar_export.sh', 'w') {
+    |f|
+    #f.puts("#!/usr/bin/env bash\n")
+    resp_obj.each{ |k,v|
+      if k =~ /^[A-Za-z0-9\.\_\-]*$/
+        f.puts("export #{envify(k)}=\"#{v}\"\n")
+      else
+        puts("invalid key: #{k}")
+      end
+    }
+  }
+  File.chmod(0744, "./sidecar_export.sh")
+else
+  exit(255)
+end
+
+
+
--- resources/sidecar/run_all.sh
+++ resources/sidecar/run_all.sh
@@ -3,13 +3,19 @@
 #http://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits/28333938#28333938
 trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
 
+$PWD/run_sidecar.sh &
+PID2=$!
+echo "PID2: $PID2"
+
+
+$PWD/export_sidecar_config.rb || {echo 'failed to export sidecar config' ; exit 211}
+. $PWD/sidecar_export.sh
+
+
 $PWD/run_pack.sh &
 PID1=$!
 echo "PID1: $PID1"
 echo "ran pack: "
-$PWD/run_sidecar.sh &
-PID2=$!
-echo "PID2: $PID2"
 
 
 while [[ ( -d /proc/$PID1 ) && ( -z `grep zombie /proc/$PID1/status` ) && ( -d /proc/$PID2 ) && ( -z `grep zombie /proc/$PID2/status` ) && ( ! -f $PWD/pack_dead ) && ( ! -f $PWD/sidecar_dead ) ]]; do
